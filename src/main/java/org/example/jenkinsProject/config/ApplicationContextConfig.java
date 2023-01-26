9cba359925357f5b630007e678a3a46852901c2a
= <apbyz@tut.by>
2023-01-26 17:30:18 +0300

    Add spring jdbc functionality

--- src/main/java/org/example/jenkinsProject/config/ApplicationContextConfig.java
+++ src/main/java/org/example/jenkinsProject/config/ApplicationContextConfig.java
@@ -3,8 +3,11 @@ package org.example.jenkinsProject.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("org.example.jenkinsProject")
@@ -16,5 +19,15 @@ public class ApplicationContextConfig {
         viewResolver.setPrefix("/WEB-INF/pages/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("tutoradmin");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/AstonInternship");
+        return dataSource;
     }
 }
